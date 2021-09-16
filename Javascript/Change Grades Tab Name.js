bc9ac6256b3079eba0a79be9a2b22b5510ba1567
Kyle Cole <kyleedwardcole@gmail.com>
2021-09-16 10:38:38 -0400

    Create Change Grades Tab Name.js

---
+++ Javascript/Change Grades Tab Name.js
@@ -0,0 +1,4 @@
+var x = document.getElementsByClassName("grades")
+for (var i = 0; i < x.length; i++) {
+    x[i].innerHTML = "new content"; ////change new content to whatever you want to display 
+    }
