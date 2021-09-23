9cd3212dad8c5043a9a3f0308b3b3d955ce6c30f
Kyle Cole <kyleedwardcole@gmail.com>
2021-09-23 09:57:23 -0400

    Delete Change Grades Tab Name.js

--- Javascript/Change Grades Tab Name.js
+++
@@ -1,4 +0,0 @@
-var x = document.getElementsByClassName("grades")
-for (var i = 0; i < x.length; i++) {
-    x[i].innerHTML = "new content"; ////change new content to whatever you want to display 
-    }
