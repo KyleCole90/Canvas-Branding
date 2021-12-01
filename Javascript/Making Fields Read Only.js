885b0c56887980aa75f8e9bfc912507c885e5f68
Kyle Cole <kyleedwardcole@gmail.com>
2021-12-01 14:03:29 -0500

    Update Making Fields Read Only.js

--- Javascript/Making Fields Read Only.js
+++ Javascript/Making Fields Read Only.js
@@ -4,4 +4,3 @@
 
 document.getElementById('user_sortable_name').readOnly = true
 document.getElementById('user_name').readOnly = true; ///This is the full name field
-$('input#course_grading_standard_enabled').prop('disabled',true); /// Grade Scheme unselectable
