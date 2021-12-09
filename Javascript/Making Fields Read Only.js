416ebac7c3d28d70a28a522e57beea4450e363fb
Kyle Cole <kyleedwardcole@gmail.com>
2021-12-09 10:45:26 -0500

    Update Making Fields Read Only.js

--- Javascript/Making Fields Read Only.js
+++ Javascript/Making Fields Read Only.js
@@ -1,6 +1,4 @@
-/////remove "///" to make the field read only/////
-
-/////User settings page/////
 
 document.getElementById('user_sortable_name').readOnly = true
 document.getElementById('user_name').readOnly = true; ///This is the full name field
+document.getElementById('course_enrollment_term_id').readOnly = true
